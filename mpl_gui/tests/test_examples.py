a5eb580c0beb50ee5df855a7e2cb52ed2ea76b74
Thomas A Caswell <tcaswell@gmail.com>
2021-04-19 01:58:21 -0400

    TST: add minimal smoke tests

--- mpl_gui/tests/test_examples.py
+++ mpl_gui/tests/test_examples.py
@@ -1,3 +1,52 @@
-def test_one_plus_one_is_two():
-    "Check that one and one are indeed two."
-    assert 1 + 1 == 2
+import sys
+import mpl_gui as mg
+
+
+def test_no_pyplot():
+
+    assert sys.modules.get("matplotlib.pyplot", None) is None
+
+
+def test_promotion():
+    fig = mg.Figure(label="test")
+    assert fig.canvas.manager is None
+    mg.show([fig], block=False)
+    assert fig.canvas.manager is not None
+
+
+def test_smoke_test_creation():
+    mg.figure()
+    mg.subplots()
+    mg.subplot_mosaic("A\nB")
+
+
+def test_smoke_test_context():
+    with mg.FigureContext(block=False) as fc:
+        fc.figure()
+        fc.subplots()
+        fc.subplot_mosaic("A\nB")
+
+
+def test_ion():
+    with mg.ion():
+        assert mg.is_interactive()
+        fig, ax = mg.subplots()
+        (ln,) = ax.plot(range(5))
+        ln.set_color("k")
+
+
+def test_ioff():
+    with mg.ioff():
+        assert not mg.is_interactive()
+
+
+def test_timeout():
+    fig = mg.Figure()
+    mg.show([fig], block=True, timeout=1)
+
+
+def test_test_context_timeout():
+    with mg.FigureContext(block=True, timeout=1) as fc:
+        fc.figure()
+        fc.subplots()
+        fc.subplot_mosaic("A\nB")
