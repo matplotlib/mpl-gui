2591db7c3182e4406f161370c90721288e82cebe
Thomas A Caswell <tcaswell@gmail.com>
2021-12-02 17:04:58 -0500

    DOC: add doc string

--- mpl_gui/__init__.py
+++ mpl_gui/__init__.py
@@ -87,6 +87,19 @@ class FigureContext:
 
     Parameters
     ----------
+    block : bool, optional
+        Whether to wait for all figures to be closed before returning.
+
+        If `True` block and run the GUI main loop until all figure windows
+        are closed.
+
+        If `False` ensure that all figure windows are displayed and return
+        immediately.  In this case, you are responsible for ensuring
+        that the event loop is running to have responsive figures.
+
+        Defaults to True in non-interactive mode and to False in interactive
+        mode (see `.is_interactive`).
+
     timeout : float, optional
         How long to block for on exit
 
