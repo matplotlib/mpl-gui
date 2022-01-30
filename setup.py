bc0cdc7bd58095039f124516cd6ca88611509513
Thomas A Caswell <tcaswell@gmail.com>
2022-01-29 19:34:46 -0500

    BLD: add classifier

--- setup.py
+++ setup.py
@@ -63,5 +63,6 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Framework :: Matplotlib',
     ],
 )
