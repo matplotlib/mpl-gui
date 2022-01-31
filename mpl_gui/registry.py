1c2e4cbcc6ee1e7e301f8167a12fe180c421e8fb
Thomas A Caswell <tcaswell@gmail.com>
2022-01-31 01:07:39 -0500

    ENH: add a module for pyplot like UX
    close #2

---
+++ mpl_gui/registry.py
@@ -0,0 +1,30 @@
+"""Reproduces the module-level pyplot UX for Figure management."""
+
+from . import FigureRegistry as _FigureRegistry
+from ._manage_backend import select_gui_toolkit
+from ._manage_interactive import ion, ioff, is_interactive
+
+_fr = _FigureRegistry()
+
+_fr_exports = [
+    "figure",
+    "subplots",
+    "subplot_mosaic",
+    "by_label",
+    "show",
+    "show_all",
+    "close",
+    "close_all",
+]
+
+for k in _fr_exports:
+    locals()[k] = getattr(_fr, k)
+
+# if one must.  `from foo import *` is a language miss-feature, but provide
+# sensible behavior anyway.
+__all__ = _fr_exports + [
+    "select_gui_toolkit",
+    "ion",
+    "ioff",
+    "is_interactive",
+]
