7ad3c9ca1684199799b141a16308bc99636f5a0a
Thomas A Caswell <tcaswell@gmail.com>
2023-10-17 18:19:14 -0400

    MNT: remove redundant config in docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -108,10 +108,6 @@ pygments_style = "sphinx"
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
-
 default_role = "obj"
 
 nitpicky = True
