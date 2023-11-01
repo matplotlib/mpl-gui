edafd11a02e332307bd793e04ba6b95eaaf0620b
Thomas A Caswell <tcaswell@gmail.com>
2023-11-01 15:07:14 -0400

    API: remove select_gui_toolkit from mpl_gui.registry
    Use the top level one still, no need to put it in multiple places

--- mpl_gui/registry.py
+++ mpl_gui/registry.py
@@ -1,8 +1,11 @@
 """Reproduces the module-level pyplot UX for Figure management."""
 
 from . import FigureRegistry as _FigureRegistry
-from ._manage_backend import select_gui_toolkit
-from ._manage_interactive import ion, ioff, is_interactive
+from ._manage_interactive import (
+    ion as ion,
+    ioff as ioff,
+    is_interactive as is_interactive,
+)
 
 _fr = _FigureRegistry()
 
@@ -32,7 +35,6 @@ def get_fignums():
 # if one must.  `from foo import *` is a language miss-feature, but provide
 # sensible behavior anyway.
 __all__ = _fr_exports + [
-    "select_gui_toolkit",
     "ion",
     "ioff",
     "is_interactive",
